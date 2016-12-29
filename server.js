e46b1e1d16393511f67a9758c5f88347872c077e
Fredy nph <cambio_fredy@hotmail.com>
2016-12-28 22:18:50 -0600

    cuatro

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_27017'],
-      mongoDatabase = process.env[mongoServiceName + '_sampledb'],
-      mongoPassword = process.env[mongoServiceName + '_solo']
-      mongoUser = process.env[mongoServiceName + '_Fredy'];
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
 
   if (mongoHost && mongoPort && mongoDatabase) {
     mongoURLLabel = mongoURL = '';
